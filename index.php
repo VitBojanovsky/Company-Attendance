64e2521a8a4662a2b26fdd3d2e876a1588031d96
Vit Bojanovsky <vit.bojanovsky@outlook.cz>
2026-02-25 10:44:52 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,53 @@
+<?php
+function loadEnv($path) {
+    if (!file_exists($path)) {
+        throw new Exception('.env file not found');
+    }
+    
+    $lines = file($path, FILE_IGNORE_NEW_LINES | FILE_SKIP_EMPTY_LINES);
+    foreach ($lines as $line) {
+        if (strpos(trim($line), '#') === 0) {
+            continue;
+        }
+        
+        list($name, $value) = explode('=', $line, 2);
+        $name = trim($name);
+        $value = trim($value);
+        
+        if (!array_key_exists($name, $_ENV)) {
+            $_ENV[$name] = $value;
+        }
+    }
+}
+
+loadEnv(__DIR__ . '/.env');
+
+$servername = $_ENV['DB_SERVERNAME'];
+$username = $_ENV['DB_USERNAME'];
+$password = $_ENV['DB_PASSWORD'];
+$dbname = $_ENV['DB_NAME'];
+
+$conn = new mysqli($servername, $username, $password);
+
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+$sql = "CREATE DATABASE IF NOT EXISTS " . $dbname;
+if ($conn->query($sql) === TRUE) {
+    echo "Database created successfully or already exists.<br>";
+} else {
+    echo "Error creating database: " . $conn->error;
+    exit;
+}
+
+$conn->close();
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if ($conn->connect_error) {
+    die("Connection to database failed: " . $conn->connect_error);
+}
+
+echo "Connected to database successfully!<br>";
+?>
